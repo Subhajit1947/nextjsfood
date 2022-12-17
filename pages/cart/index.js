5adaea17217b46171b5361ee569158dc2d4f38b3
Subhajit Das <dassubhajit@gmail.com>
2022-12-18 00:46:53 +0530

    some file added

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -2,20 +2,21 @@ import React from 'react'
 import piza from '../../public/piza2.png'
 import Image from 'next/image'
 import { connect } from 'react-redux'
+import abcd from '../../styles/Index.module.css'
 function cart({cart1,removecart}) {
     let sum=0
     
     for(let i=0;i<cart1.length;i++){
         sum+=cart1[i].price*cart1[i].qty
     }
-    
+    console.log(abcd.maincart)
   return (
-    <div style={{width:'100vw',display:'flex'}}>
+    <div >
         {cart1.length!=0?
-        <>
-        <div style={{width:'70%'}}>
+        <div className={abcd.maincart} >
+        <div className={abcd.tablec} >
             
-            <table style={{width:'100%',borderSpacing:'20px'}}>
+            <table>
                 <thead>
                     <tr>
                         <th>Product</th>
@@ -46,7 +47,7 @@ function cart({cart1,removecart}) {
             
 
         </div>
-        <div style={{height:'12rem',width:'20%',backgroundColor:'black',margin:'1rem',color:'white',paddingLeft:'2rem',paddingTop:'1rem'}}>
+        <div className={abcd.totalno} >
             <h2>CART TOTAL</h2>
             <br/>
             
@@ -58,7 +59,7 @@ function cart({cart1,removecart}) {
             <br/>
             <button style={{width:'80%',fontWeight:'bold',height:'2rem'}}>checkout now</button>
         </div>
-        </>
+        </div>
         :
         <div style={{height:'10rem',width:'100%',display:'flex',justifyContent:'center',alignItems:'center',color:'red'}}>
             <h1>NO ITEM IN CART</h1>
